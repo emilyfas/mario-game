703fe6d33e7609b64f480c70286864132551b887
Emilly Fernandes <emilly.fernandesads@gmail.com>
2023-05-06 16:41:36 -0300

    adcionando arquivos

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
+
+const overDiv = document.querySelector(".game-status");
+var counterVal = 0;
+
+const jump = () =>{
+    mario.classList.add("jump")
+
+    setTimeout(() => {
+        mario.classList.remove("jump")
+    } , 500);
+
+    updateDisplay(++counterVal);
+
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 105 && pipePosition > 0 && marioPosition < 80){
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+        mario.src = '/images/game-over.png';
+        mario.style.width = '65px';
+        mario.style.marginLeft = '35px';
+
+        clearInterval(loop)
+
+        gameOver();
+    }
+}, 10);
+
+function gameOver() {
+    overDiv.innerHTML += `<img src="images/overpic.png" alt="imagem game over" class="game-over">
+    <button class="buttonStart" onclick="start()">
+        <img src="images/Daco_4422541.png" alt="imagem começar jogo" width="150px" class="start">
+    </button>`
+}
+
+
+function start(){
+    location.reload();
+}
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
+
+document.addEventListener('keydown', jump);
