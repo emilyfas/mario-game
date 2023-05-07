76f7a9f0c68df6d1927027c018429e40b1bf9239
Emilly Fernandes <emilly.fernandesads@gmail.com>
2023-05-07 11:59:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ const loop = setInterval(() => {
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
-        mario.src = '/images/game-over.png';
+        mario.src = 'images/game-over.png';
         mario.style.width = '65px';
         mario.style.marginLeft = '35px';
 
