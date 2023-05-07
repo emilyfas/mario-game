30b09813c5b73805eb2e46f31d672edd5ac05fa3
Emilly Fernandes <emilly.fernandesads@gmail.com>
2023-05-07 19:43:48 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,7 +14,6 @@ const jump = () =>{
         mario.classList.remove("jump")
     } , 500);
 
-    updateDisplay(++counterVal);
 
 }
 
@@ -36,7 +35,11 @@ const loop = setInterval(() => {
         clearInterval(loop)
 
         gameOver();
+    } else if (pipePosition <= 105 && pipePosition > 0 && marioPosition > 80){    
+        updateDisplay(++counterVal);
     }
+    
+    
 }, 10);
 
 function gameOver() {
